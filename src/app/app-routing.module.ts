5dc82ebbb7bf50c977f825260cd97e861644f55c
Mikboch <mikolaj.bochinski@innotion.pl>
2022-04-18 22:21:01 +0200

    Initial login system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
+import {MainPageComponent} from "./components/main-page/main-page.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'main-page', canActivate:[AuthGuard] , component: MainPageComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
