9ef62bcf5c8884716cdcd080219d0b6aab87944e
Piotr Chojnowski <40302552+piotr107@users.noreply.github.com>
2022-04-19 21:38:28 +0200

    init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/apsi-work-tracking-frontend'),
+      dir: require('path').join(__dirname, './coverage/apsi-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { Observable, of, throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Consts } from '../consts/consts';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+
+  authenticated = false;
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+  }
+
+  login({ email, password }: any): Observable<any> {
+    const headers = new HttpHeaders({
+      authorization : 'Basic ' + btoa(email + ':' + password)
+    });
+    var response: any;
+
+    this.http.get(Consts.BACKEND_URL + 'user', {headers: headers}).subscribe((response : any) => {
+      if (response['name']) {
+          this.authenticated = true;
+          // get token
+          this.setToken('abcdefghijklmnopqrstuvwxyz');
+          response = response;
+      } else {
+          this.authenticated = false;
+      }
+    });
+
+    if (this.authenticated) {
+      return of({ name: response['name'], email: response['email'] });
+    } else {
+      return throwError(new Error('Failed to login'));
+    }
+  }
+}
