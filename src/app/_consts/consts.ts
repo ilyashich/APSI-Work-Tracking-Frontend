eebf758e1a6fef7eef35b12f1cf91625390ec328
Piotr Chojnowski <40302552+piotr107@users.noreply.github.com>
2022-06-29 16:26:26 +0200

    Update consts.ts

--- src/app/_consts/consts.ts
+++ src/app/_consts/consts.ts
@@ -1,3 +1,3 @@
 export class Consts {
-    public static BACKEND_URL='https://apsi-work-tracking-backend.azurewebsites.net/apsi/api/';
+    public static BACKEND_URL='https://apsi-work-tracking-backend-prod.azurewebsites.net/apsi/api/';
  }
